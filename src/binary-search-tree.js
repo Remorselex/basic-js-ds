e5e8a17876b046278c4c2b969c3db8e9994760d4
Oleg Ostrovskiy <remorselex99@gmail.com>
2022-10-18 00:03:49 +0300

    feat: binary tree completed

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
@@ -8,42 +8,168 @@ const { NotImplementedError } = require('../extensions/index.js');
 */
 class BinarySearchTree {
 
-  root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	BinarySearchTree() {
+		this._root = null;
+	}
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	root() {
+		if (!this._root) {
+			return null;
+		}
+		return this._root;
+	}
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	add(data) {
+		const newNode = new Node(data);
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+		if (!this._root) {
+			this._root = newNode;
+		} else {
+			treeDiveAdd(this._root, newNode);
+		}
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+		function treeDiveAdd(currentNode, newNode) {
+			// left
+			if (newNode.data < currentNode.data) {
+				if (!currentNode.left) {
+					currentNode.left = newNode;
+					return;
+				}
+				treeDiveAdd(currentNode.left, newNode);
+			}
+			//right
+			if (newNode.data > currentNode.data) {
+				if (!currentNode.right) {
+					currentNode.right = newNode;
+					return;
+				}
+				treeDiveAdd(currentNode.right, newNode);
+			}
+		}
+	}
 
-  min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	has(data) {
+		return treeDiveHas(data, this.root());
 
-  max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+		function treeDiveHas(data, currentNode) {
+			if (!currentNode) {
+				return false;
+			}
+			if (data == currentNode.data) {
+				return true;
+			}
+
+			if (data < currentNode.data) {
+				return treeDiveHas(data, currentNode.left);
+			}
+			if (data > currentNode.data) {
+				return treeDiveHas(data, currentNode.right);
+			}
+		}
+	}
+
+	find(data) {
+		return treeDiveFind(data, this.root());
+
+		function treeDiveFind(data, currentNode) {
+			if (!currentNode) {
+				return null;
+			}
+			if (data == currentNode.data) {
+				return currentNode;
+			}
+
+			if (data < currentNode.data) {
+				return treeDiveFind(data, currentNode.left);
+			}
+			if (data > currentNode.data) {
+				return treeDiveFind(data, currentNode.right);
+			}
+		}
+	}
+
+	remove(data) {
+		this._root = treeDiveRemove(data, this.root());
+
+		function treeDiveRemove(data, currentNode) {
+			if (!currentNode) {
+				return null;
+			}
+
+			if (data < currentNode.data) {
+				currentNode.left = treeDiveRemove(data, currentNode.left);
+				return currentNode;
+			}
+			else if (data > currentNode.data) {
+				currentNode.right = treeDiveRemove(data, currentNode.right);
+				return currentNode;
+			}
+			else if (data == currentNode.data) {
+				if (currentNode.left === null && currentNode.right === null) {
+					currentNode = null;
+					return currentNode;
+				}
+
+				if (currentNode.left === null) {
+					currentNode = currentNode.right;
+					return currentNode;
+				}
+				else if (currentNode.right === null) {
+					currentNode = currentNode.left;
+					return currentNode;
+				}
+				var subtree = minInSubtree(currentNode.right);
+				currentNode.data = subtree.data;
+
+				currentNode.right = treeDiveRemove(subtree.data, currentNode.right);
+
+				return currentNode;
+			}
+		}
+
+		function minInSubtree(currentNode) {
+			if (!currentNode) {
+				return null;
+			}
+
+			if (!currentNode.left) {
+				return currentNode;
+			}
+			return minInSubtree(currentNode.left);
+		}
+	}
+
+	min() {
+		return treeDiveMin(this.root());
+
+		function treeDiveMin(currentNode) {
+			if (!currentNode) {
+				return null;
+			}
+
+			if (currentNode.left) {
+				return treeDiveMin(currentNode.left);
+			}
+			return currentNode.data;
+		}
+	}
+
+	max() {
+		return treeDiveMin(this.root());
+
+		function treeDiveMin(currentNode) {
+			if (!currentNode) {
+				return null;
+			}
+
+			if (currentNode.right) {
+				return treeDiveMin(currentNode.right);
+			}
+			return currentNode.data;
+		}
+	}
 }
 
 module.exports = {
-  BinarySearchTree
+	BinarySearchTree
 };
